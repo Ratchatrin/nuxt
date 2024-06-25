53641fb93410fedd19d420cd778290a47922fb80
Ratchatrin sisan <148691205+Ratchatrin@users.noreply.github.com>
2024-06-26 00:14:34 +0700

    delete pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  plugins: [
-    '~/plugins/pinia.js',
-  ],
   devtools: { enabled: true },
   css: ['/main.css'],
   postcss: {
